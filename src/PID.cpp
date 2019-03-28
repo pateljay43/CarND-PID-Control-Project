58f121be8c093b729c657a4fa5def56d417af6d9
pateljay43 <pateljay143@gmail.com>
2019-03-28 11:20:23 +0530

    initial commit

---
+++ src/PID.cpp
@@ -0,0 +1,46 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+  // p_error right now if previous cte
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  double steer = Kp * p_error + Kd * d_error + Ki * i_error;
+//   if (steer < -1) {
+//     steer = -1;
+//   }
+//   if (steer > 1) {
+//     steer = 1;
+//   }
+  return steer;
+}
